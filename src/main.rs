119205ec3861d309c1488e65eb6232a83c34d223
Michael Scholten <michael-scholten@hotmail.nl>
2025-02-28 11:15:31 +0100

    Used clap to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::{env::args, fs::File, io::BufReader, time::Instant};
-
+use clap::Parser as _;
 use elf_parser::Elf;
+use std::{fs::File, io::BufReader, path::PathBuf, time::Instant};
+
+/// This application Parses the ELF file passed as argument.
+#[derive(Debug, clap::Parser)]
+struct Args {
+    /// The ELF file to parse
+    file: PathBuf,
+}
 
 fn main() {
+    let args = Args::parse();
     let start = Instant::now();
     let elf = Elf::from_readable(&mut BufReader::new(
-        File::open(args().nth(1).expect("Expected an ELF file as argument"))
-            .expect("Passed argument is not a file"),
+        File::open(args.file).expect("Passed argument is not a file"),
     ))
     .expect("Parsed argument is not a valid ELF file");
     let performance = start.elapsed();
