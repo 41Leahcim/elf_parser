0f73f03f19fc220c5c31c85b430558a31060b553
Michael Scholten <michael-scholten@hotmail.nl>
2025-02-28 11:10:50 +0100

    The application now takes an ELF file as argument instead of always reading itself

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::{env::args, fs::File, io::BufReader, time::Instant};
 
 use elf_parser::Elf;
 
-#[expect(clippy::unwrap_used)]
 fn main() {
     let start = Instant::now();
     let elf = Elf::from_readable(&mut BufReader::new(
-        File::open(args().next().unwrap()).unwrap(),
+        File::open(args().nth(1).expect("Expected an ELF file as argument"))
+            .expect("Passed argument is not a file"),
     ))
-    .unwrap();
+    .expect("Parsed argument is not a valid ELF file");
     let performance = start.elapsed();
     println!("{elf:?}\n{elf}",);
     println!("{performance:?}");
